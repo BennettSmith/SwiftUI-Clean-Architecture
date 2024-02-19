29332483a159ec18335af64a0155832376cb1827
Bennett Smith <bennettsmith@mac.com>
2024-02-18 19:57:57 -0800

    refactor: adds a presenter abstraction to the use case (#1)
    * The PokemonModel got renamed to PokemonListItem. Think of it as a response model from the use case.
    * Moved the PokemonListItem into the use case layer. It is part of the "application", not part of the presentation layer.
    * Introduced a PokemonListPagePresenter closure to the use case. It acts as the abstract presenter for the use case.
    * Modified the execute method of the use case to accept a presenter.
    * Modified the execute method of the use case to call the map from the entites to response models.
    * Modfiied the execute method to call the presenter closure with the response models.

--- PokeDex/FeatureExplore/Domain/UseCases/GetPokemonListUseCase.swift
+++ PokeDex/FeatureExplore/Domain/UseCases/GetPokemonListUseCase.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 class GetPokemonListUseCase {
+    typealias PokemonListPagePresenter = @MainActor (_ offset: Int, _ items: [PokemonListItem]) -> Void
     let repository: ExploreRepositoryProtocol
     
     init(pokeDexRepository: ExploreRepositoryProtocol) {
         self.repository = pokeDexRepository
     }
     
-    func execute(limit: Int, offset: Int) async throws -> [PokemonEntity] {
-        return try await repository.fetchPokemons(limit: limit, offset: offset)
+    func execute(limit: Int = 20, offset: Int, presenter presentPokemonListPage: @escaping PokemonListPagePresenter) async throws -> Void {
+        let entities = try await repository.fetchPokemons(limit: limit, offset: offset)
+        let models = entities.compactMap({ entity in PokemonListItem(pokemon: entity)})
+        await presentPokemonListPage(offset, models)
     }
 }
---
+++ PokeDex/FeatureExplore/Presentation/Models/README.md
@@ -0,0 +1,3 @@
+Relocated the `PokemonModel` into the `Domain/UseCases` folder. This model maps to the output port used by a presenter. It is not UI/feature specific.
+
+
